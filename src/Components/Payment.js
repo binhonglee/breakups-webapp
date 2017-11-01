d28ecddb336ac5a51f0ee98dbc7075f06e81143e
BinHong Lee <binhong@binhong.me>
2017-10-31 23:28:41 -0700

    Improve PaymentChain look by a little

--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -5,7 +5,8 @@ class Payment extends Component {
         return (
             <div className="Payment">
                 <br/>
-                {this.props.payment.from} ----{this.props.payment.amount}----> {this.props.payment.to}
+                {this.props.payment.from} -> {this.props.payment.to}<br/>
+                Amount: {parseFloat(this.props.payment.amount).toFixed(2)}
             </div>
         )
     }
