3b1038dbe9e820112e813212c31a6ae083bfd12e
BinHong Lee <binhong@binhong.me>
2017-12-27 01:03:25 -0700

    Fix useless constructor

--- src/Components/People.js
+++ src/Components/People.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 class People extends Component {
-    constructor() {
-        super();
-    }
-
     render() {
         return (
             <div className="People">
