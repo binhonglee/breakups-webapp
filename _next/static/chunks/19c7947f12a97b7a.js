6eba70d218c31614c2687ee78caba487788e7c68
binhonglee <binhonglee@users.noreply.github.com>
2025-10-23 01:51:17 +0000

    deploy: 6d6693b6331d5e9fe5d76b0b623fdd6ac3e805e9

---
+++ _next/static/chunks/19c7947f12a97b7a.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/e17a4b87fd6162d9.js",
+  "static/chunks/115081b48667fe43.js",
+  "static/chunks/1a040f06f10cda2d.js",
+  "static/chunks/turbopack-a70b372aa598750a.js"
+])
---
+++ _next/static/lqAfG2Il1o8YOv1OKjyIV/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/19c7947f12a97b7a.js"
+  ],
+  "/_error": [
+    "static/chunks/d1be25f25d006377.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
